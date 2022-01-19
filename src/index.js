a99c507778ce4ab51c1a011dd7a70402d11d65fe
Valentin Puchko <puchxp@rambler.ru>
2022-01-19 16:28:08 +0300

    feat: done js task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = n.toString();
+    let i = 0;
+    let result = '';
+        while (i <= str.length - 1) {
+        result = `${str[i]}${result}`;
+        i++;
+    };
+    return parseInt(result);
+};
